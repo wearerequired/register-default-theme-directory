964214d5946ab44464d228d1e1ad8bcb674a8449
Dominik Schilling <dominikschilling+git@gmail.com>
2016-12-14 23:22:34 +0100

    Initial commit.

---
+++ register-theme-directory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Register Default Theme Directory
+ * Description: Registers the theme directory for default WordPress themes.
+ * Version:     1.0.0
+ * Author:      required
+ * Author URI:  https://required.com/
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+register_theme_directory( ABSPATH . 'wp-content/themes' );
